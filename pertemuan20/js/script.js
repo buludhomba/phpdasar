1f43cd6e3e987c4bd237db0ae652c81d15cbb9c5
Sella Putri Sari <74763171+buludhomba@users.noreply.github.com>
2022-01-25 10:49:35 +0700

    sama kayak di vid, cmn addEventListenes gk disa kbaca

--- pertemuan20/js/script.js
+++ pertemuan20/js/script.js
@@ -1,9 +1,12 @@
 // ambil elemen2 yg dibtuhkan
 var keyword = document.getElementById('keyword');
-  if( keyword) {
-      keyword.addEventListener('keyup', function() {
+var tombolCari = document.getElementById('tombol-cari');
+var container = document.getElementById('container');
 
-        // buat objek ajax
+// tambhkn event ktika keyword ditulis
+keyword.addEventListener('keyup', function() {
+    
+    // buat objek ajax
     var xhr = new XMLHttpRequest();
 
     // cek kesiapan ajax
@@ -17,28 +20,4 @@ var keyword = document.getElementById('keyword');
     xhr.open('GET', 'ajax/mahasiswa.php?keyword=' +keyword.value, true);
     xhr.send();
 
-      });
-
-  }
-
-var tombolCari = document.getElementById('tombol-cari');
-var container = document.getElementById('container');
-
-// // tambhkn event ktika keyword ditulis
-// keyword.addEventListener('keyup', function() {
-    
-//     // buat objek ajax
-//     var xhr = new XMLHttpRequest();
-
-//     // cek kesiapan ajax
-//     xhr.onreadystatechange = function() {
-//         if( xhr.readyState == 4 && xhr.status == 200 ) {
-//             container.innerHTML = xhr.responseText;
-//         }
-//     }
-
-//     // eksekusi ajax
-//     xhr.open('GET', 'ajax/mahasiswa.php?keyword=' +keyword.value, true);
-//     xhr.send();
-
-// });
+});
