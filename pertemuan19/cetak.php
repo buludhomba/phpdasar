4cb2a821bab55c5fad3883678b1105e5f104f610
ardanvf <71808926+ardanvf@users.noreply.github.com>
2022-01-28 15:06:12 +0700

    Tombol Cetak

--- pertemuan19/cetak.php
+++ pertemuan19/cetak.php
@@ -9,42 +9,17 @@ if( !isset($_SESSION["login"]) ) {
 
 require 'functions.php';
 $mahasiswa = query("SELECT * FROM mahasiswa ");
-
-
-// tombol cari diklik kita akan menimpa nya data mhssw
-if( isset($_POST["cari"]) ) {
-    $mahasiswa = cari($_POST["keyword"]);
-}
-
 ?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Admin</title>
+    <title>Cetak Daftar Mahasiswa</title>
 </head>
 <body>
-
-<a href="logout.php">Logout</a>
-
 <h1>Daftar Mahasiswa</h1>
-
-<a href="tambah.php">Tambah data mahasiswa</a>
-<br><br>
-
-
-<form action="" method="post">
-
-    <input type="text" name="keyword" size="40" autofocus placeholder="masukkan keyword pencarian...." autocomplete="off" id="keyword">
-    <button type="submit" name="cari" id="tombol-cari">Cari</button>
-    <a href="cetak.php" target="_BLANK">TES PRINT</a>
-  
-</form>
 
 <br>
 <div id="container">
@@ -53,7 +28,6 @@ if( isset($_POST["cari"]) ) {
     
     <tr>
         <th>No.</th>
-        <th>Aksi</th>
         <th>Gambar</th>
         <th>Nama</th>
         <th>NIM</th>
@@ -65,10 +39,6 @@ if( isset($_POST["cari"]) ) {
     <?php foreach( $mahasiswa as $row ) : ?>
     <tr>
         <td><?= $i ?></td>
-        <td>
-            <a href="ubah.php?id=<?php echo $row["id"]; ?>">Ubah</a> |
-            <a href="hapus.php?id=<?php echo $row["id"]; ?>" onclick="return confirm('yakin?');">hapus</a> 
-        </td>
         <td><img src="img/<?php echo $row["gambar"]; ?> " alt="" widht="50" height="50"></td>
         
         <td><?= $row["nama"]; ?></td>
@@ -82,6 +52,9 @@ if( isset($_POST["cari"]) ) {
 </table>
 </div>
 
-<script src="js/script.js"></script>
+    <script>
+		window.print();
+	</script>
+
 </body>
 </html>
